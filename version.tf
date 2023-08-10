637c74ee2658f53ddb2d8411f8041fd77440a536
Brian Ojeda <9335829+sgtoj@users.noreply.github.com>
2023-08-10 14:19:23 +0000

    test: lint tf files

--- version.tf
+++ version.tf
@@ -6,5 +6,9 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.0.0, < 6.0.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
   }
 }
